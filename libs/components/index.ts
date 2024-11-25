edf456f13a3a0cdc1374a05d88a1bb92b7982364
Christian Illies (FINATIX) <86478641+cillies-finatix@users.noreply.github.com>
2024-11-25 13:00:54 +0100

    update SME/Tom app (#62)
    * Style tom app
    
    Add AdvertisementCard
    Add routes for create, list of demands
    apply styles for dackl tabs
    Add customt dackl tablist
    Apply auto-load by using watchers
    
    * Add stepper to create demands
    
    * feat: Move tabs to shared ui components
    
    * Moved components and composables to shared
    
    * Styled Service Provider step
    
    * Styled Create Demand step
    
    * Fix minor dropdown positions
    
    * Finish Create Demand Styles
    
    * Finish Active Loan Page
    
    * Fix bugs in Demands
    
    * Remove Service component
    
    * Enhance UX by rerouting and change sorting
    
    * Minor styling fixes in create demand
    
    * Scroll into card with same amount if it is set
    
    * Scroll into card after accepting an offer
    
    * Add Dackl Header Bar
    
    * fix: use red label for terminated demands
    
    * Add Reload Button to Page heade
    
    * fix: prevent invalid number input values

--- libs/components/index.ts
+++ libs/components/index.ts
@@ -1,15 +1,26 @@
 import AccessRequestCallback from './src/AccessRequestCallback.vue';
 import AuthAppHeaderBar from './src/AuthAppHeaderBar.vue';
+import CheckMarkSvg from './src/CheckMarkSvg.vue';
 import DateFormatted from './src/DateFormatted.vue';
 import HeaderBar from './src/HeaderBar.vue';
+import DacklHeaderBar from './src/DacklHeaderBar.vue';
+import HorizontalLine from "./src/HorizontalLine.vue";
 import LDN from './src/LDN.vue';
 import LDNs from './src/LDNs.vue';
 import LoginButton from './src/LoginButton.vue';
 import LogoutButton from './src/LogoutButton.vue';
+import PageHeadline from "./src/PageHeadline.vue";
 import SignUpButton from './src/SignUpButton.vue';
+import SmeCard from "./src/SmeCard.vue";
+import SmeCardHeadline from "./src/SmeCardHeadline.vue";
+import TabList from './src/tabs/TabList.vue';
+import DacklTextInput from './src/DacklTextInput.vue';
+
+export * from './src/tabs/TabItemType';
 
 export {
     HeaderBar,
+    DacklHeaderBar,
     AuthAppHeaderBar,
     LoginButton,
     LogoutButton,
@@ -17,5 +28,12 @@ export {
     DateFormatted,
     LDN,
     LDNs,
-    AccessRequestCallback
-}
+    AccessRequestCallback,
+    CheckMarkSvg,
+    TabList,
+    HorizontalLine,
+    SmeCard,
+    PageHeadline,
+    SmeCardHeadline,
+    DacklTextInput,
+};
---
+++ libs/components/src/DacklTextInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-column gap-2">
+    <label v-if="label" class="text-sm relative z-1 pl-2 text-black-alpha-70" :for="id">{{ label }}</label>
+    <InputText :inputmode="type === 'number' ? 'numeric' : 'text'" class="pt-5 -mt-5" :id="id" :value="modelValue" @keyup="update($event.target.value)" />
+    <span v-show="error" class="text-red-500">Ungültige Eingabe</span>
+  </div>
+</template>
+<script setup lang="ts">
+import {ref} from "vue";
+
+const props = defineProps<{ type: string; modelValue: string; label: string; }>()
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void,
+}>();
+const error = ref<boolean>(false);
+
+const id = Math.random().toString(32).substring(2);
+
+function update(value: string): void {
+  if (props.type === 'number') {
+    const isValidNumber = !Number.isNaN(Number(value));
+    error.value = !isValidNumber;
+    // Do not emit invalid values
+    if (error.value) {
+      return;
+    }
+  }
+  emit('update:modelValue', value);
+}
+</script>
+<style scoped>
+label {
+  top: 0.3rem;
+}
+</style>
